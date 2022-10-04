7cf8555bc346773b3f13e22a97175919f6929466
Thomas Much <github@snailshell.de>
2022-10-04 12:05:00 +0200

    Extract domain code

---
+++ src/main/java/a_code/Berechnung.java
@@ -0,0 +1,18 @@
+package a_code;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Berechnung {
+
+    private static final LocalDate STICHTAG = LocalDate.of(2023, Month.JANUARY, 1);
+
+    public int ermittleWert(LocalDate heute) {
+
+        if (heute.isBefore(STICHTAG)) {
+            return 123;
+        } else {
+            return 456;
+        }
+    }
+}
---
+++ src/test/java/a_code/BerechnungTest.java
@@ -0,0 +1,50 @@
+package a_code;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class BerechnungTest {
+
+    @Test
+    void ermittleWert_liefert_Standardwert_vor_Stichtag() {
+
+        // Given
+
+        Berechnung berechnung = new Berechnung();
+
+        LocalDate heute = LocalDate.of(2022, Month.OCTOBER, 6);
+
+        // When
+
+        int wert = berechnung.ermittleWert(heute);
+
+        // Then
+
+        assertThat(wert).isEqualTo(123);
+    }
+
+    @Test
+    void ermittleWert_liefert_höheren_Wert_ab_Stichtag() {
+
+        // Given
+
+        Berechnung berechnung = new Berechnung();
+
+        LocalDate heute = LocalDate.of(2023, Month.JANUARY, 1);
+
+        // When
+
+        int wert = berechnung.ermittleWert(heute);
+
+        // Then
+
+        assertThat(wert).isEqualTo(456);
+    }
+}
