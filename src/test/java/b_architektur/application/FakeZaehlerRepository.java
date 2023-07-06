db44e7b4179decd926f2b81513eda413adff4ccf
Thomas Much <github@snailshell.de>
2023-07-06 11:20:41 +0200

    Add some fake logic (instead of returning a fixed stubbed value)

--- src/test/java/b_architektur/application/FakeZaehlerRepository.java
+++ src/test/java/b_architektur/application/FakeZaehlerRepository.java
@@ -3,9 +3,18 @@ package b_architektur.application;
 import b_architektur.domain.model.Kundennummer;
 import b_architektur.domain.service.ZaehlerRepository;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class FakeZaehlerRepository implements ZaehlerRepository {
+
+    private final AtomicLong zaehler = new AtomicLong(1000L);
+
     @Override
     public Kundennummer getNextKundennummer() {
-        return new Kundennummer(1234L);
+        return new Kundennummer(zaehler.incrementAndGet());
+
+        // oder einfach einen festen Wert liefern ("stub" statt "fake"),
+        // falls das für die Tests ausreicht:
+        // return new Kundennummer(1234L);
     }
 }
